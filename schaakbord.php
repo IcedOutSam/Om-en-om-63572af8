82e0cdc98194255ebd23e40b870b932173292915
Sam <2102926@talnet.nl>
2020-04-28 17:16:11 +0200

    Uitwerking van de exercise

---
+++ schaakbord.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Thugz mansion</title>
+
+  </head>
+
+  <body>
+
+<table>
+  <?php
+for ($tr = 0; $tr <= 8; $tr++) {
+
+    echo "<tr>";
+
+
+    for ($td = 1; $td <= 9; $td++) {
+        $optimusPrime = $tr + $td;
+        if ($optimusPrime % 2 == 1) {
+            echo "<td class=blokwit height=50px width=50px bgcolor=#FF0000></td>";
+        } else {
+            echo "<td class=blokzwart height=50px width=50px bgcolor=#000000></td>";
+        }
+
+    }
+    echo "</tr>";
+
+}
+?>
+</table>
+
+  </body>
+</html>
