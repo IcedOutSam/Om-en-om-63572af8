46410ba2141bba29b3a4337b9bbb2f411603d7f7
Sam <2102926@talnet.nl>
2020-04-29 23:15:00 +0200

    Uitwerking van de exercise

--- schaakbord.php
+++ schaakbord.php
@@ -9,14 +9,14 @@
   <?php
   for ($tr = 0; $tr <= 8; $tr++) {
       echo "<tr>";
-      for ($td = 1; $td <= 9; $td++) {
+        for ($td = 1; $td <= 9; $td++) {
             $optimusPrime = $tr + $td;
-          if ($optimusPrime % 2 == 1) {
+            if ($optimusPrime % 2 == 1) {
                 echo "<td class=blokwit height=50px width=50px bgcolor=#FF0000></td>";
-          } else {
+            } else {
                 echo "<td class=blokzwart height=50px width=50px bgcolor=#000000></td>";
-            }
-      }
+          }
+       }
       echo "</tr>";
   }
   ?>
